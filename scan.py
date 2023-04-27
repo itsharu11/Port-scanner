663aff8a1f555ef3134cc8fc5bb9980a464e3154
itsharu11 <itsharu.dev@gmail.com>
2023-04-27 12:13:47 +0530

    Made Basic Scanner

---
+++ scan.py
@@ -0,0 +1,36 @@
+import pyfiglet
+import sys
+import socket
+from datetime import datetime
+
+ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
+print(ascii_banner)
+
+target = input(str("Target IP: "))
+
+#Banner
+print("_" * 50)
+print("Scanning Started at: " + str(datetime.now()))
+print("_" * 50)
+
+try :
+
+    #Scan Ports
+
+    for port in range(1,65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(0.5)
+
+        #Retrun Open Port
+        result = s.connect_ex((target,port))
+        if result == 0:
+            print("[*] Port {} is open ".format(port))
+        s.close()
+
+except KeyboardInterrupt:
+        print("\n Exiting :(")
+        sys.exit()
+except socket.error:
+        print("\ Host not responding :()")
+        sys.exit()
+
